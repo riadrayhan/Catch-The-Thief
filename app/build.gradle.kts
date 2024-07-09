fbb336a5ce0f3eecdbc203a24c1b5bd1f8f4b59f
Riad <riadrayhan111@gmail.com>
2024-07-09 14:43:15 +0600

    new commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.image_detector"
+    namespace = "com.example.techlab"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.image_detector"
+        applicationId = "com.example.techlab"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
